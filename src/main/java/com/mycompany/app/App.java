50dba88e07fbbca948586a605aaf71c246fffd05
dyeray <dyeray@gmail.com>
2016-04-03 15:16:40 +0200

    Google Cloud Java Client basic example

---
+++ src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,34 @@
+package com.mycompany.app;
+
+import static java.nio.charset.StandardCharsets.UTF_8;
+import com.google.gcloud.storage.StorageOptions;
+import com.google.gcloud.storage.Storage;
+import com.google.gcloud.storage.Blob;
+import com.google.gcloud.storage.BlobId;
+import com.google.gcloud.storage.BlobInfo;
+import com.google.gcloud.AuthCredentials;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+
+public class App 
+{
+    /*
+       params:
+         args[0]: path to service account private key (json format)
+         args[1] bucket name
+      */
+
+    public static void main( String[] args )
+    {
+        try {
+            Storage storage = StorageOptions.builder().authCredentials(AuthCredentials.createForJson(new FileInputStream(args[0]))).build().service();
+            BlobId blobId = BlobId.of("yeray-cloud", "helloworld.txt");
+            BlobInfo blobInfo = BlobInfo.builder(blobId).contentType("text/plain").build();
+            Blob blob = storage.create(blobInfo, "Hello, Cloud Storage!".getBytes(UTF_8));
+        }
+        catch(IOException ex){
+            System.out.println(ex.getMessage());
+        }
+    }
+}
